e4cd9ecea7b608ad2d8c42e584aabc8af7abe370
Matt Ao <m.ao@techsmith.com>
2015-02-12 14:28:33 -0500

    Cleanup transition, new image for popup

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,11 +1,12 @@
 // Inline popups
 $('#inline-popup').magnificPopup({
-  removalDelay: 300, //delay removal by X to allow out-animation
+  removalDelay: 800, //delay removal by X to allow out-animation
   callbacks: {
     beforeOpen: function() {
        this.st.mainClass = this.st.el.attr('data-effect');
     }
   },
+  closeBtnInside: false,
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 
