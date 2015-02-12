e363fbaa0309d0bf5f8d59c5439e7b6d01f52201
Matthew Ao <mattyayoh@gmail.com>
2015-02-11 22:27:42 -0500

    Magnific Popup Skeleton Addition

---
+++ extension/js/popup.js
@@ -0,0 +1,11 @@
+// Inline popups
+$('#inline-popup').magnificPopup({
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
